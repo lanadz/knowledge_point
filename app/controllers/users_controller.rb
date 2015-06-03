7d9c437eb23584feaeaf710684fc2f09842dd268
Svitlana Dzyban <svitlana.dzyuban@gmail.com>
2015-06-03 18:35:22 +0300

    Add Users controller, view, routes.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def new
+
+  end
+
+  def update
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+end
